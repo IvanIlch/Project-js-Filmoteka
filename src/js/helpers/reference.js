98e9cd2ecd0ecde511469a51b2c21128198fc2e9
Marik713 <62308344+Marik713@users.noreply.github.com>
2020-11-16 01:30:32 +0200

    header-js

--- src/js/helpers/reference.js
+++ src/js/helpers/reference.js
@@ -1,3 +1,12 @@
 refs = {
-    
+    logo: document.querySelector('.site-logo'),
+    header: document.querySelector('.header'),
+    navLinkHome: document.querySelector('.site-navigation__link '),
+    headerButtons: document.querySelector('.header-buttons'),
+    burger: document.querySelector('.hamburger'),
+    overlayHeader: document.querySelector('.overlay'),
+    nav: document.querySelector('.page-nav'),
+    searchForm: document.querySelector('.search-form'),
+    siteNav: document.querySelector('.site-navigation')
+
 }
