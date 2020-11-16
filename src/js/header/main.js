3459f3ac9c86f3683d62c53d3dd1fa2ba90263f1
Ivan <ilchenkoivan92@gmail.com>
2020-11-16 15:11:46 +0200

    Додав js в header

--- src/js/header/main.js
+++ src/js/header/main.js
@@ -1,15 +1,61 @@
 refs.logo.addEventListener('click', () => {
-    refs.header.classList.add('header-home');
-    refs.navLinkHome.classList.add('site-site-navigation__link--active');
-
+    classListHome();
+    setHomeActive();
 });
+refs.burger.addEventListener('click', clickOnBurger);
+refs.navLinkLibrary.addEventListener('click', (e) => {
+    classListLibrary()
+    if (refs.navLinkHome.classList.contains('site-navigation__link--active')) {
+        refs.navLinkHome.classList.remove('site-navigation__link--active');
+        refs.navLinkLibrary.classList.add('site-navigation__link--active');
+    }
+})
+refs.navLinkHome.addEventListener('click', (e) => {
+    classListHome()
+    if (refs.navLinkLibrary.classList.contains('site-navigation__link--active')) {
+        setHomeActive();
+    }
+})
+refs.buttonHomeOnBurger.addEventListener('click', () => {
+    classListHome();
+    closeBurger()
+})
+refs.buttonLibraryOnBurger.addEventListener('click', () => {
+    classListLibrary();
+    closeBurger();   
+})
+function classListHome() {
+    refs.header.classList.remove('header-library');
+    refs.header.classList.add('header-home');
+    refs.searchForm.classList.remove('is-visible');
+    refs.headerButtons.classList.add('is-visible');
+    refs.headerButtons.classList.remove('flex');
+}
+function classListLibrary() {
+    refs.header.classList.remove('header-home');
+    refs.header.classList.add('header-library');
+    refs.searchForm.classList.add('is-visible');
+    refs.headerButtons.classList.remove('is-visible');
+    refs.headerButtons.classList.add('flex')
 
-refs.burger.addEventListener('click', () => {
+}
+function setHomeActive() {
+    refs.navLinkLibrary.classList.remove('site-navigation__link--active');
+    refs.navLinkHome.classList.add('site-navigation__link--active');
+}
+function clickOnBurger() {
     refs.overlayHeader.classList.toggle('is-visible');
     refs.burger.classList.toggle('is-active');
     refs.nav.classList.toggle('flex');
     refs.nav.classList.toggle('is-visible');
     refs.searchForm.classList.toggle('is-visible');
     refs.siteNav.classList.toggle('is-visible');
-
-});
+    refs.headerButtons.classList.add('is-visible');
+    refs.headerButtons.classList.remove('flex');
+}
+function closeBurger() {
+    refs.burger.classList.remove('is-active');
+    refs.nav.classList.remove('is-visible');
+    refs.nav.classList.add('flex');
+    refs.overlayHeader.classList.add('is-visible');
+}
--- src/js/helpers/reference.js
+++ src/js/helpers/reference.js
@@ -1,12 +1,17 @@
 refs = {
     logo: document.querySelector('.site-logo'),
     header: document.querySelector('.header'),
-    navLinkHome: document.querySelector('.site-navigation__link '),
+    navLinkHome: document.querySelector('.home-js'),
+    navLinkLibrary: document.querySelector('.library-js'),
     headerButtons: document.querySelector('.header-buttons'),
     burger: document.querySelector('.hamburger'),
     overlayHeader: document.querySelector('.overlay'),
     nav: document.querySelector('.page-nav'),
     searchForm: document.querySelector('.search-form'),
-    siteNav: document.querySelector('.site-navigation')
+    siteNav: document.querySelector('.site-navigation'),
+    buttonWatched: document.querySelector('.button-watched-js'),
+    buttonQueue: document.querySelector('.button-queue-js'),
+    buttonHomeOnBurger: document.querySelector('.site-navigation-on-burger__link-home-js'),
+    buttonLibraryOnBurger: document.querySelector('.site-navigation-on-burger__link-library-js')
 
 }
