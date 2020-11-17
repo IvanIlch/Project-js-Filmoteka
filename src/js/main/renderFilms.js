c919efd7b08719d6b4e055017dc748f216aef365
Volodymyr Molodynia <molodunja91@gmail.com>
2020-11-17 02:05:16 +0200

    відредагував стилі додав ul в hbs

--- src/js/main/renderFilms.js
+++ src/js/main/renderFilms.js
@@ -1,22 +1,10 @@
-import articlesTpl from "./templates/articles.hbs"
-import refs from "../helpers/reference"
-
+import articlesTpl from './templates/articles.hbs';
+import refs from '../helpers/reference';
 
 function updateArticlesMarcup(articles) {
-    const marcup = articlesTpl(articles);
-  
-    refs.galleryList.insertAdjacentHTML('beforeend', marcup);
-  }
-  
-  export default updateArticlesMarcup;
+  const marcup = articlesTpl(articles);
 
-//  const axios = require('axios');
-
-
-const apiKey = '18753901-48fe4160ef12c9ad3fe1a3bef'
-
-function fechArticles(serchQuery){
-   return axios.get(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${serchQuery}&page=1&per_page=12&key=${apiKey}`)
-    .then(({data})=>data.hits)
+  refs.galleryList.insertAdjacentHTML('beforeend', marcup);
 }
-export default fechArticles
+
+export default updateArticlesMarcup;
